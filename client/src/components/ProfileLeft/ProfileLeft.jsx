a971b5e6945a65846dff5799a21e447b8dfa69d1
khoa25200 <91365201+khoa25200@users.noreply.github.com>
2023-02-07 20:11:31 +0700

    ui01

--- client/src/components/ProfileLeft/ProfileLeft.jsx
+++ client/src/components/ProfileLeft/ProfileLeft.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import FollowersCard from '../FollowersCard/FollowersCard'
-import InfoCard from '../InfoCard/InfoCard'
-import LogoSearch from '../LogoSearch/LogoSearch'
-
+import React from "react";
+import FollowersCard from "../FollowersCard/FollowersCard";
+import InfoCard from "../InfoCard/InfoCard";
+import LogoSearch from "../LogoSearch/LogoSearch";
+import { useDevice } from "react-use-device";
 const ProfileLeft = () => {
+  const { isMOBILE } = useDevice();
   return (
-    <div className="ProfileSide">
-        <LogoSearch/>
-        <InfoCard/>
-        <FollowersCard/>
-    </div>
-  )
-}
+    !isMOBILE && (
+      <div className="ProfileSide">
+        <LogoSearch />
+        <InfoCard />
+        <FollowersCard />
+      </div>
+    )
+  );
+};
 
-export default ProfileLeft
+export default ProfileLeft;
